ef97d814e7e697632e77d59d41b02747b9da79a6
Rishi Raja <rishisraja@gmail.com>
2020-10-03 15:04:25 -0700

    Initial Commit

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+/**
+ * This class represents an Adding Machine which can add and subtract values.
+ */
+public class AddingMachine {
+    /**
+     * The total value after performing the arthimetics
+     */
+    private int total;
+
+    /**
+     * This is the contructor for the class.
+     * Sets the value of total to 0
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * This method gets the total value.
+     * @return int The value 0
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * This method adds the values.
+     * @param value The number to be added.
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * This method subtracts the values.
+     * @param value The number to be subtracted.
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * This method prints out the history of inputs.
+     * @return String The value "".
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * This method clears the memory.
+     */
+    public void clear() {
+    }
+}
